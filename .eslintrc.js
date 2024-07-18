c4bfd44f4709f5e2c9fc47fff44781cae895ffda
Sergio Gómez Bachiller <sergio.gomez@audiense.com>
2024-07-18 08:28:49 +0200

    chore: config styles

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+const { resolve } = require('node:path');
+
+const project = resolve(__dirname, 'tsconfig.json');
+
+module.exports = {
+  root: true,
+  extends: [
+    require.resolve('@vercel/style-guide/eslint/node'),
+    require.resolve('@vercel/style-guide/eslint/typescript'),
+  ],
+  overrides: [
+    {
+      files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+      extends: [require.resolve('@vercel/style-guide/eslint/vitest')],
+    },
+  ],
+  parserOptions: {
+    project,
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        project,
+      },
+    },
+  },
+};
